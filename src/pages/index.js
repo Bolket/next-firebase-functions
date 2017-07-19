857fc36ea0fe05cb42928052a95ccb384eba7939
sarovin <sarovin86@gmail.com>
2017-07-19 16:45:30 +0200

    ADD 2 pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,4 +4,16 @@ import Link from 'next/link';
 export default () =>
   <div>
     <h1>NextJS Server Side</h1>
+    <ul>
+      <li>
+        <Link href="/a">
+          <a>a</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/b">
+          <a>b</a>
+        </Link>
+      </li>
+    </ul>
   </div>;
