5ce79597300febb588786e515b5ec932745cac79
sarovin <sarovin86@gmail.com>
2017-08-08 11:30:10 +0200

    add functions dir

---
+++ src/functions/index.js
@@ -0,0 +1,24 @@
+import * as functions from 'firebase-functions';
+import cors from 'cors';
+import express from 'express';
+import next from 'next';
+
+const nextApp = next({ dev: false, conf: { distDir: 'next' } });
+const handle = nextApp.getRequestHandler();
+
+const slasher = handler => (req, res) => {
+  if (!req.path) {
+    req.url = `/${req.url}`;
+  }
+
+  return handler(req, res);
+};
+
+export let app = functions.https.onRequest(
+  slasher((req, res) => {
+    return nextApp.prepare().then(() => handle(req, res)).catch(ex => {
+      console.error(ex.stack);
+      process.exit(1);
+    });
+  })
+);
